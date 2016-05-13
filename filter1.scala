20bd4a79ce0ccc0674910d36333da68f572a6618
Neel Smith <dnsmith.neel@gmail.com>
2016-05-13 18:42:30 -0400

    Factor out collecting source text

--- filter1.scala
+++ filter1.scala
@@ -6,30 +6,21 @@ import scala.io.Source
 
 object PandocJsonFilter {
 
+
+  def srcText(args: Array[String]): String = {
+    if (args.size < 1) scala.io.Source.stdin.getLines mkString "\n"
+    else Source.fromFile(args(0)).getLines mkString "\n"
+  }
+
   // takes name of a source file in pandoc JSON,
   // does something with it
   def main(args: Array[String]) = {
-    if (args.size < 1) {
-      val text = scala.io.Source.stdin.getLines mkString "\n"
-      val parsedVal = parse(text)
+    val text = srcText(args)
+    val parsedVal = parse(text)
 
-
-      println ("\npeek at yaml metadata:")
-      val yaml = parsedVal \\ "unMeta"
-      println (pretty(render(yaml)))
-
-
-    } else {
-      val src = Source.fromFile(args(0))
-      val srcStr = src.getLines mkString "\n"
-      val parsedVal = parse(srcStr)
-      //println (pretty(render(parsedVal)))
-
-
-      println ("\npeek at yaml metadata:")
-      val yaml = parsedVal \\ "unMeta"
-      println (pretty(render(yaml)))
-    }
+    println ("\npeek at yaml metadata:")
+    val yaml = parsedVal \\ "unMeta"
+    println (pretty(render(yaml)))
   }
 
 
