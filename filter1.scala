61f3dc3e158f7a1043c29ba6b3d578eedbac57a0
Neel Smith <dnsmith.neel@gmail.com>
2016-05-13 20:42:04 -0400

    Add some scaladocs.

--- filter1.scala
+++ filter1.scala
@@ -4,22 +4,36 @@ import org.json4s.native.JsonMethods._
 
 import scala.io.Source
 
+/**
+*/
 object PandocJsonFilter {
 
 
+  /** Gets a string of JSON data as a string.
+  * If command-line args included, first arg is
+  * considered to be a file name.  Otherwise,
+  * uses stdin as source for JSON text.
+  * @param args Command-line args.
+  * @return String of JSON text.
+  */
   def srcText(args: Array[String]): String = {
     if (args.size < 1) scala.io.Source.stdin.getLines mkString "\n"
     else Source.fromFile(args(0)).getLines mkString "\n"
   }
 
-  // takes name of a source file in pandoc JSON,
-  // does something with it
-  def main(args: Array[String]) = {
-    val text = srcText(args)
-    val parsedVal = parse(text)
 
-    println ("\npeek at yaml metadata:")
-    val yaml = parsedVal \\ "unMeta"
+  /** Transforms Pandoc JSON with URN values to pandoc
+  * Pandoc JSON with URNs resolved to URLs.
+  * @param args Zero or one command-line args.  If
+  * arg included, it is assumed to be the name of a
+  * file with Pandoc JSON.
+  */
+  def main(args: Array[String]) = {
+
+    val parsed = parse(srcText(args))
+
+    println ("Peek at yaml metadata:")
+    val yaml = parsed \\ "unMeta"
     println (pretty(render(yaml)))
   }
 
